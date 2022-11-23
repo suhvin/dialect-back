a7e917cb0e0265d315d058cc745de097e8dfbd8e
Yang Jaehoon <didwogns789@yonsei.ac.kr>
2022-11-23 10:43:21 +0900

    Init

---
+++ api/views.py
@@ -0,0 +1,9 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET', 'POST'])
+def hello_world(request):
+    if request.method == 'POST':
+        print(request.data)
+        return Response({"message": "Got some data!", "data": request.data})
+    return Response({"message": "Hello, world!"})
