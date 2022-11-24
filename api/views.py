3c3e123834896b125fb6de10395f98a337d08efc
Yang Jaehoon <didwogns789@yonsei.ac.kr>
2022-11-24 21:20:45 +0900

    Change real host

--- api/views.py
+++ api/views.py
@@ -8,19 +8,20 @@ load_dotenv()
 @api_view(['POST'])
 def send_sms(request):     
     if request.method == 'POST':
-        print(request.data)
+        
+        pay_msg = request.data.PCD_PAY_MSG
 
         account_sid = os.getenv('TWILIO_ACCOUNT_SID')
         auth_token = os.getenv('TWILIO_AUTH_TOKEN')
         client = Client(account_sid, auth_token)
 
         client.messages.create(
-            body='Hi there',
-            from_='+15136476298',
+            body=pay_msg,
+            from_=os.getenv('PHONE_NUMBER'),
             to='+821040302748'
         )
 
+        # print(request.data)
         # print(message._properties)
         
         return Response({"message": "Got some data!", "data": request.data})
-    return Response({"message": "Hello, world!"})
