97c24481a4a3263989ec4cdc50ed2720b7c7c2cd
suhvin <suh.kiseok.new@gmail.com>
2022-11-25 16:25:29 +0900

    221125

--- api/views.py
+++ api/views.py
@@ -9,19 +9,21 @@ load_dotenv()
 def send_sms(request):     
     if request.method == 'POST':
         
+        print("test")
+        print(request.data) # PCD_PAY_RST
         data = request.data['PCD_PAY_RST'] # success
+        print(data)
         # data = request.data['PCD_PAY_MSG'] # 카드승인완료
-        # print(request.data) # PCD_PAY_RST
 
-        account_sid = os.getenv('TWILIO_ACCOUNT_SID')
-        auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+        account_sid = "AC0d50f72d5b699de30c6d2a7a84f823e5" # os.getenv('TWILIO_ACCOUNT_SID')
+        auth_token = "616c4481cbe4915ece4887ec54cd1203" #os.getenv('TWILIO_AUTH_TOKEN')
         client = Client(account_sid, auth_token)
 
         client.messages.create(
             # body="Hi there!",
             body=data,
-            from_=os.getenv('PHONE_NUMBER'),
-            to='+821040302748'
+            from_="+19498326476",#os.getenv('PHONE_NUMBER'),
+            to='+821085319070'
         )
 
         # print(request.data)
