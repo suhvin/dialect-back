311e975a485ae93726553c884faa4d69eb0f97c2
suhvin <suh.kiseok.new@gmail.com>
2022-11-25 16:42:45 +0900

    22112516

--- scheduler/jobs.py
+++ scheduler/jobs.py
@@ -7,12 +7,24 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-def do_something():
+def do_something1():
     # Use the application default credentials
     # cred = credentials.Certificate("/content/drive/MyDrive/secret key/yeonpick-0727-firebase-adminsdk-41a1r-57f7d55c29.json")
     cred = credentials.Certificate(os.getcwd() + "/scheduler/firebase-secret.json")
+
     try:
         firebase_admin.initialize_app(cred)
-        print("Yes!!")
+        print("something1")
     except:
-        print("Already Connected")
+        print("Already Connected something1")
+
+def do_something2():
+    # Use the application default credentials
+    # cred = credentials.Certificate("/content/drive/MyDrive/secret key/yeonpick-0727-firebase-adminsdk-41a1r-57f7d55c29.json")
+    cred = credentials.Certificate(os.getcwd() + "/scheduler/firebase-secret.json")
+
+    try:
+        firebase_admin.initialize_app(cred)
+        print("something2")
+    except:
+        print("Already Connected something2")
--- scheduler/updater.py
+++ scheduler/updater.py
@@ -1,9 +1,11 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from .jobs import do_something
+from .jobs import do_something1, do_something2
 
 
 def start():
-    scheduler = BackgroundScheduler()
-    # scheduler.add_job(do_something, 'interval', days=2)
-    scheduler.add_job(do_something, 'interval', days=2)
+    scheduler = BackgroundScheduler(timezone='Asia/Seoul')
+    # 이틀 차이나게 start_date 수정
+    scheduler.add_job(do_something1, 'cron', start_date=datetime(2022, 11, 25), day="*/2", hour="16", minute='46', id='job1')
+    scheduler.add_job(do_something2, 'cron', start_date=datetime(2022, 11, 26), day="*/2", hour="16", minute='46', id='job2')
+
     scheduler.start()
