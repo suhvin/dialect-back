bd20730789d1c7b0a7246e568cc8fb4ceeab227c
Yang Jaehoon <didwogns789@yonsei.ac.kr>
2022-11-25 11:54:08 +0900

    Chage data

--- api/views.py
+++ api/views.py
@@ -10,7 +10,7 @@ def send_sms(request):
     if request.method == 'POST':
         
         # data = request.data['PCD_PAY_RST'] # success
-        data = request.data['PCD_PAY_MSG']
+        data = request.data['PCD_PAY_MSG'] # 카드승인완료
         # print(request.data) # PCD_PAY_RST
 
         account_sid = os.getenv('TWILIO_ACCOUNT_SID')
